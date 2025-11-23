106e35650658679897ee2bc2aecec399a4be8792
Pedniel <d.pedder@hotmail.de>
2025-11-23 17:03:19 +0100

    Add more tests
    Still very simple tests, but enough to verify functionality.

--- src/modcntr/tb_top.v
+++ src/modcntr/tb_top.v
@@ -1,3 +1,7 @@
+`define CLK_CYCLE 10
+`define CLK_HALF_CYCLE 5
+
+
 module tb_top;
    
    reg clk;
@@ -9,20 +13,49 @@ module tb_top;
    initial begin
       clk = 0;
       clr = 0;
-      forever #5 clk = ~clk;
+      forever #`CLK_HALF_CYCLE clk = ~clk;
    end
 
    initial begin
       reset_n = 1'b1;
-      #10
+      #(10*`CLK_CYCLE)
       reset_n = 1'b0;
    end
 
    initial begin
-      test_1();
       $monitor("pulse=%h \n", pulse_o);
+      test_1();
+      #(32*`CLK_CYCLE)
+      test_2();
+      #(6*`CLK_CYCLE)
+      test_3();
    end
-   
+
+   task test_1();
+      begin
+         top <= 16'h0F;
+      end
+   endtask // test_1
+
+   task test_2();
+      begin      
+         top = 16'h03;
+         clr = 1'b1;
+         #(1*`CLK_CYCLE)
+         clr = 1'b0;
+      end
+   endtask // test_2
+
+   task test_3();
+      begin
+         top = 16'hFF;
+         clr = 1'b1;
+         #(1*`CLK_CYCLE)
+         clr = 1'b0;
+      end
+   endtask // test_3
+
+
    Modcntr dut(
                .clk(clk),
                .clr_in(clr),
@@ -30,10 +63,6 @@ module tb_top;
                .reset(reset_n),
                .pulse_out(pulse_o)
                );
-
-   task test_1();
-      top <= 16'h0F;
-   endtask
    
 endmodule
  
