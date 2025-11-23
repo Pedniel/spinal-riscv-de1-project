e056d7a5242c9107de7e2a5427f79860d6023b66
Pedniel <d.pedder@hotmail.de>
2025-11-23 15:40:27 +0100

    Rename io signals and remove top register
    `Top` register is not needed to compare to `top_in`.
    Instead we handle this with a `clr` signal, which resets the counter
    when pulled to `High`.
    
    We also added `setName` after all the input signals, so they have a
    non-autogenerated name in verilog compile and simulation.
    
    This commit also adds the compiled verilog code of the SpinalHDL Modcounter

--- src/modcntr/modcnt.scala
+++ src/modcntr/modcnt.scala
@@ -4,17 +4,16 @@ import spinal.lib._
 class Modcntr extends Component {
 
   val io = new Bundle {
-    val top_in = in port UInt(16 bits)
-    val pulse = out port Bool()
+    val top_in = in port UInt(16 bits) setName("top_in")
+    val clr_in = in port Bool() setName("clr_in")
+    val pulse = out port Bool() setName("pulse_out")
   }
   
-  val top = Reg(UInt(16 bits)) init (0) setName("top")
-  val ctr = Reg(UInt(16 bits)) init (0) setName("ctr")
+  val ctr = Reg(UInt(16 bits)) init (0) setName("ctr") 
   
-  top := io.top_in
   io.pulse := False  
 
-  when (top =/= io.top_in) {
+  when (io.clr_in === True) { 
     ctr := 0
   } elsewhen (ctr === io.top_in) {
     ctr := 0
@@ -24,3 +23,7 @@ class Modcntr extends Component {
   }
   
 }
+
+object MyDesignVerilog extends App {
+  SpinalVerilog(new Modcntr)
+} 
