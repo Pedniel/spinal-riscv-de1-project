141484b3adaaead0b4be1068cd79bd32013c5588
Pedniel <d.pedder@hotmail.de>
2025-11-27 19:44:58 +0100

    Fix compilation errors and add LED output
    Starting from this commit, the goal will be to get this build running on
    an FPGA and handle the LEDR output.
    
    Once this works, actual sigma delta frequency divider logic will be implemented.

--- src/SigDeltaFreqDiv.scala
+++ src/SigDeltaFreqDiv.scala
@@ -9,23 +9,49 @@ case class SigDeltaFreqDiv(val int, div : Int) extends Component {
   val wordCount = (byteArray.length+3)/4 // Found in MuraxUtiles.scala
   val io = new Bundle{
     val apb = slave(Apb3(log2Up(wordCount*4),32)) // addressWidth, dataWidth
+    val KEY0     = in Bool() setName("LED_KEY")
+    val LEDR     = out Bits(8 bits) setName("LED_OUT")                                 
   }
 
   val apbWrite = io.apb.PSEL && io.apb.PENABLE && io.apb.PWRITE
   val apbRead  = io.apb.PSEL && io.apb.PENABLE && !io.apb.PWRITE
 
-  val freq_int = Reg(Uint(16 bits)) init (0) setName("freq_int") 
-  val freq_div = Reg(Uint(8 bits))  init (0) setName("freq_div")
+  val freq_int = Reg(UInt(16 bits)) init (0) setName("freq_int") 
+  val freq_div = Reg(UInt(8 bits))  init (0) setName("freq_div")
   val freq_ctr = Reg(UInt(16 bits)) init (0) setName("freq_ctr")
 
-  // Reg Addr offset mapping
+  // Default value assignmnents
+  io.LEDR := B"8'x00"
+  io.apb.PRDATA := 0
+
+  // Reg Addr offset mapping for READ and WRITE 
   when(apbWrite) {
     switch(io.apb.PADDR) {
-      is(DIV) { div := io.apb.PWDATA(8 downto 0) }
-      is(INT) { int := io.apb.PWDATA(15 downto 0) }
+      is(U(DIV, 4 bits)) {
+        freq_div := io.apb.PWDATA(7 downto 0).asUInt
+      }
+      is(U(INT, 4 bits)) {
+        freq_int := io.apb.PWDATA(15 downto 0).asUInt
+      }
     }
-
   }
+
+  when(apbRead) {
+    switch(io.apb.PADDR) {
+      is(U(DIV, 4 bits)) {
+        io.apb.PRDATA := freq_div.asBits.resize(32)
+      }
+      is(U(INT, 4 bits)) {
+        io.apb.PRDATA := freq_int.asBits.resize(32)
+      }
+    }
+  }
+
+  // LED Handling
+  when(io.KEY0) {
+    io.LEDR := B"8'xFF"
+  }
+}
   
   /////////////////////////////////////////////////
   // Apb Summary for me :                         //
