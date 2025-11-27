76f84ad34e97b62634a00f3329a5ce8cc7011f9d
Pedniel <d.pedder@hotmail.de>
2025-11-27 21:20:14 +0100

    Fix to first functional version
    This can be considered the first functional version.
    It compiles and in general should work. However, none of the frequency
    divider logic is implemented yet.

--- src/SigDeltaFreqDiv.scala
+++ src/SigDeltaFreqDiv.scala
@@ -1,24 +1,30 @@
 import spinal.core._
 import spinal.lib._
+import spinal.lib.bus.amba3.apb.{Apb3, Apb3Config, Apb3SlaveFactory}
 
 // Register addr offest
 object SigmaDeltaReg {
-  val DIV  = 0x00  
-  val INT  = 0x04  
+  val DIV         = 0x00  
+  val INT         = 0x04
+  val addressWidth = 4 // highest offest
+  val dataWidth   = 32
 }
 
-case class SigDeltaFreqDiv(val int : UInt, val div : UInt) extends Component {
+class SigDeltaFreqDiv extends Component {
   import SigmaDeltaReg._
-  val wordCount = (byteArray.length+3)/4 // Found in MuraxUtiles.scala
   val io = new Bundle{
-    val apb = slave(Apb3(log2Up(wordCount*4),32)) // addressWidth, dataWidth
+    val apb = slave(Apb3(addressWidth, dataWidth)) // addressWidth, dataWidth
     val KEY0     = in Bool() setName("LED_KEY")
     val LEDR     = out Bits(8 bits) setName("LED_OUT")                                 
   }
 
-  val apbWrite = io.apb.PSEL && io.apb.PENABLE && io.apb.PWRITE
-  val apbRead  = io.apb.PSEL && io.apb.PENABLE && !io.apb.PWRITE
+  io.apb.PREADY := True // Always ready for now
+  io.apb.PSLVERROR := False // Never error for now
+  
+  val apbWrite = io.apb.PSEL.orR && io.apb.PENABLE && io.apb.PWRITE
+  val apbRead  = io.apb.PSEL.orR && io.apb.PENABLE && !io.apb.PWRITE
 
+  
   val freq_int = Reg(UInt(16 bits)) init (0) setName("freq_int") 
   val freq_div = Reg(UInt(8 bits))  init (0) setName("freq_div")
   val freq_ctr = Reg(UInt(16 bits)) init (0) setName("freq_ctr")
@@ -57,22 +63,33 @@ case class SigDeltaFreqDiv(val int : UInt, val div : UInt) extends Component {
 }
 
 object MyDesignVerilog extends App {
-  SpinalVerilog(new SigDeltaFreqDiv)
+  SpinalVerilog(new SigDeltaFreqDiv())
+}
+
+object MyDesignVhdl extends App {
+  SpinalVhdl(new SigDeltaFreqDiv())
 } 
 
-  /////////////////////////////////////////////////
-  // Apb Summary for me :                         //
-  //                                              //
-  //  PSEL    : 1 = SELECTED                      //
-  //                                              //
-  //  PENABLE : 1 for DATA incoming               //
-  //                                              //
-  //  PWRITE  : 1 = Write, 0 = Read               //
-  //                                              //
-  //  PADDR   : Byte offset for Reg map           //
-  //                                              //
-  //  PWDATA  : Write -> Data from bus to me      //
-  //                                              //
-  //  PRDATA  : Read ->  Data from me to bus      //
-  /////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  // Apb Summary for me :                                          //
+  //                                                               //
+  //  PSEL    : 1 = SELECTED                                       //
+  //                                                               //
+  //  PENABLE : 1 for DATA incoming                                //
+  //                                                               //
+  //  PWRITE  : 1 = Write, 0 = Read                                //
+  //                                                               //
+  //  PADDR   : Byte offset for Reg map                            //
+  //                                                               //
+  //  PWDATA  : Write -> Data from bus to me                       //
+  //                                                               //
+  //  PRDATA  : Read ->  Data from me to bus                       //
+  //  Documentation found here:                                    //
+  //https://wifasoi.github.io/SpinalDoc/spinal/lib/bus/amba3/apb3/ //
+  //                                                               //
+  //  To create VHDL/Verilog files, run:                           //
+  //  sbt "runMain MyDesignVhdl"                                   //
+  //  or                                                           //
+  //  sbt "runMain MyDesignVerilog"                                //
+  ///////////////////////////////////////////////////////////////////
 
