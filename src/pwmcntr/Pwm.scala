fb6b139583dabab1eeb780076cf4fe33abeeea3a
Pedniel <d.pedder@hotmail.de>
2025-11-29 22:47:54 +0100

    Update PWM to fit the RP2350 datasheet
    After rereading the datasheet, I found multiple differences.
    
    Mainly:
    
    -Top was used for the full PWM period. In my implementation, top only
    defined the HIGH period.
    
    -CC was used for the HIGH period definition.
    
    -pulse_o is purely combinational logic. The register I used was unnecessary.

--- src/pwmcntr/Pwm.scala
+++ src/pwmcntr/Pwm.scala
@@ -4,40 +4,33 @@ import spinal.lib._
 class Pwm extends Component {
 
   val io = new Bundle {
-    val top_in = in port UInt(16 bits) setName("top_in")
-    val ctr_in = in port UInt(16 bits) setName("ctr_in")
+    val top_in = in port UInt(16 bits) setName("top_in") // Defines the whole PWM period
+    val cc_in = in port UInt(16 bits) setName("cc_in")
     val clr_in = in port Bool() setName("clr_in")
     val pulse_o = out port Bool() setName("pulse_out")
   }
 
   // Registers
   val ctr = Reg(UInt(16 bits)) init (0) setName("ctr") 
-  val on_off_switch = Reg(Bool()) init (True) setName("on_off_switch")
 
   // Functions
-  def switch_bool(value: Bool): Bool = !value
-  def TURN_PULSE_ON(): Bool = ((ctr === io.top_in) && on_off_switch)
-  def TURN_PULSE_OFF(): Bool = ((ctr === io.ctr_in) && !on_off_switch)
+  def PWM_CYCLE_LENGTH_REACHED(): Bool = (ctr === io.top_in)
 
-  // For some reason, when assigning the switch to pulse directly,
-  // I get type errors? Maybe a scala thing. TODO
-  when (on_off_switch === True) {
+  // Pulse combinational logic
+  when (io.cc_in > io.top_in) { // RP2350 Sheet Page 1080 " A CC value of TOP + 1 (..) produces a 100% output"
     io.pulse_o := True
+  } elsewhen (ctr >= io.cc_in) {
+    io.pulse_o := False    
   } otherwise {
-    io.pulse_o := False
+    io.pulse_o := True
   }
   
   // FSM(ish)
   when (io.clr_in === True) { 
     ctr := 0
     
-  } elsewhen (TURN_PULSE_ON()) {
+  } elsewhen (PWM_CYCLE_LENGTH_REACHED()) {
     ctr := 0
-    on_off_switch := switch_bool(on_off_switch)
-    
-  } elsewhen (TURN_PULSE_OFF()) {
-    ctr := 0
-    on_off_switch := switch_bool(on_off_switch)
     
   } otherwise {
     ctr := ctr + 1    
