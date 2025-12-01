a7515a5262cc68ade0767fc87f9fbb97240ee54a
Pedniel <d.pedder@hotmail.de>
2025-12-01 21:22:37 +0100

    Update simple FreqDiv+PWM testbench
    This testbench so far work with the frequency divider set to 2.5 and a
    full PWM cycle of 15 enable ins.
    
    The high part, controlled by cc_in, is 5 enable ins long.

--- src/pwmcntr/FreqDivSim.scala
+++ src/pwmcntr/FreqDivSim.scala
@@ -1,19 +1,24 @@
 import spinal.core.sim._
 import spinal.core._
 import spinal.lib._
-import pwm._
+import pwmcore._
 
 object FreqDivSim extends App {
-  SimConfig.compile(new PwmSystem).doSim {
+  SimConfig
+    .withWave 
+    .withConfig(
+      SpinalConfig(defaultClockDomainFrequency = FixedFrequency(100 MHz))
+    )
+    .doSim(new pwmcore.PwmSystem()) {
     dut =>
     
     dut.clockDomain.forkStimulus(period = 5)
     dut.io.div_config #= 37 //37 -> 00000010 0101 --> 2,5
     dut.clockDomain.waitSampling(1)
 
-    dut.io.pwm_cc_in #= 15
-    dut.io.pwm_top_in #= 30
-    dut.clockDomain.waitSampling(200)
+    dut.io.pwm_cc_in #= 5 
+    dut.io.pwm_top_in #= 15
+    dut.clockDomain.waitSampling(500)
 //    pwm.io.clr_in #= false
 
     simSuccess()
