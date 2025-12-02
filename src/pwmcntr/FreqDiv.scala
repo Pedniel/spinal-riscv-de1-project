078a23d3904942fe4551343b049f20ec102b17e4
Pedniel <d.pedder@hotmail.de>
2025-12-02 13:12:17 +0100

    FreqDiv: Adjust INT calculation to fit RP2350 specsheet
    To fit the specsheet of the RP2350, which describes this:
    
    "Set DIV_INT to 0 to divide the count rate by the maximum possible value
    of 256. You must not set any DIV_FRAC bits when DIV_INT is 0" (see RP2350 datasheet, page 1084)
    
    We have to use `div_in - 1` for our comparison with `ctr`.

--- src/pwmcntr/FreqDiv.scala
+++ src/pwmcntr/FreqDiv.scala
@@ -3,21 +3,20 @@ import spinal.core._
 import spinal.lib._
 
 class FreqDiv() extends Component {
-  
   val io = new Bundle {
     val div_in = in UInt(12 bits) 
     val en_o   = out Bool()       
   }
 
+  // Registers
   val frac_acc = Reg(UInt(5 bits)) init(0) 
-//  val div_int  = Reg(UInt(8.bits)) init(0)
   val ctr      = Reg(UInt(8 bits)) init(0) 
-//io.div_in(11 downto 4)
+
   // FSM
   when (frac_acc.msb === True) {
     frac_acc := (False ## frac_acc(3 downto 0)).asUInt
     io.en_o := False
-  } elsewhen (ctr === (io.div_in >> 4)) {
+  } elsewhen (ctr === (io.div_in >> 4) - 1) {
     ctr := 0
     io.en_o := True
     frac_acc := frac_acc(3 downto 0) +^ io.div_in(3 downto 0)
