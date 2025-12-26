0400a196950f9ea8c56f7c048e873ad6883ea124
Daniel Pedder <d.pedder@hm.edu>
2025-12-26 02:37:06 +0100

    Connect pwmSystem to apb
    Instead of using the regluar input/output signals we rewrite pwmSystem
    to connect over apb bus.

--- VexRiscv/src/main/scala/vexriscv/demo/PwmSystem.scala
+++ VexRiscv/src/main/scala/vexriscv/demo/PwmSystem.scala
@@ -2,61 +2,46 @@ package vexriscv.demo
 import spinal.core._
 import spinal.lib._
 import spinal.lib.bus.amba3.apb.{Apb3, Apb3Config, Apb3SlaveFactory}
+import spinal.lib.bus.misc.BusSlaveFactory
 
 class PwmSystem() extends Component{
   
   val io = new Bundle {
     val apb = slave(Apb3(Apb3Config(addressWidth=8, dataWidth=32)))
-    val pwm_top_in  = in UInt(16 bits)  
-    val pwm_cc_in   = in UInt(16 bits)
-    val pwm_clr_in   = in Bool()
-    val div_config  = in UInt(12 bits) 
-    val pulse_o     = out Bool()
+    val pulse_o = out Bool()
     val ctr_freqdiv_o = out UInt(8 bits)
-
-    // Bus signals
-    val limit = in UInt(width bits)
-
   }
 
-  val busCtrl = Apb3SlaveFactory(io.apb)
-
   val divider  = new FreqDiv()
-
   val pwm = new Pwm()
 
   io.ctr_freqdiv_o := divider.io.ctr_o
-    
-  pwm.io.top_in     := io.pwm_top_in
-  pwm.io.cc_in      := io.pwm_cc_in
-  pwm.io.clr_in     := io.pwm_clr_in
-  
-  divider.io.div_in := io.div_config
-  
   io.pulse_o        := pwm.io.pulse_o
 
-  def driveFrom(busCtrl: BusSlaveFactory, baseAddress: BigInt)(ticks: Seq[Bool], clears: Seq[Bool]) = new Area {
-    // Offset 0 => clear/tick masks + bus
-    val pwmEnable = busCtrl.createReadAndWrite(Bits(ticks.length bits), baseAddress + 0,0) init(0)
-    val clrEnable = busCtrl.createReadAndWrite(Bits(clears.length bits), baseAddress + 0,16) init(0)
-    val busClearing = False
+  val busCtrl = Apb3SlaveFactory(io.apb)
 
-    pwm.io.clr_in := (clrEnable & clears.asBits).orR | busClearing
-    pwm.io.en_in := (pwmEnable & divider.io.en_o)
-    //io.tick := (pwmEnable  & ticks.asBits ).orR
+  driveFromBus(busCtrl, 0x00)
 
-    // Offset 4 => read/write limit (+ auto clear)
-    busCtrl.driveAndRead(io.limit, baseAddress + 4)
-    busClearing.setWhen(busCtrl.isWriting(baseAddress + 4))
+  def driveFromBus(busCtrl: BusSlaveFactory, baseAddress: BigInt) = new Area {
+    val pwmTop = busCtrl.createReadAndWrite(UInt(16 bits), baseAddress + 0x00) init(U"h000F")  // 16 bits
+    pwm.io.top_in := pwmTop
 
-    // Offset 8 => read timer value / write => clear timer value
-    busCtrl.read(io.value, baseAddress + 8)
-    busClearing.setWhen(busCtrl.isWriting(baseAddress + 8))
+    val pwmCompare = busCtrl.createReadAndWrite(UInt(16 bits), baseAddress + 0x04) init(U"h0005") // 16 bits
+    pwm.io.cc_in := pwmCompare
+
+    val divConfig = busCtrl.createReadAndWrite(UInt(12 bits), baseAddress + 0x08) init(U"b000000100101") // 12 bits
+    divider.io.div_in := divConfig
+
+    val control = busCtrl.createReadAndWrite(Bits(32 bits), baseAddress + 0x0C) init(B"h00000001")
+    val pwmEnable = control(0)
+    val pwmClear = control(1)
+
+    pwm.io.en_in := pwmEnable & divider.io.en_o
+    pwm.io.clr_in := pwmClear
+
   }
 }
 
-}
-  
 object PwmSystem{  
   def main(args: Array[String]) {
     SpinalVerilog(new PwmSystem())
